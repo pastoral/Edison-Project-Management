d054444afe80c3161d764f20d2604695eca1f4b5
pastoral <munir.hoque@gmail.com>
2016-03-29 01:23:04 +0800

    reirect_to functionality added

--- app/controllers/sw_versions_controller.rb
+++ app/controllers/sw_versions_controller.rb
@@ -15,7 +15,7 @@ class SwVersionsController < ApplicationController
 		#@swversion = SwVersion.new(params[:swversion])
 		@swversion = SwVersion.new(sw_version_params)
 		@swversion.save
-		#redirect_to @swversion
+		redirect_to @swversion
 	end
 
 	private
